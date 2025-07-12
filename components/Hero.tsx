6cbc537ec39a741c66a403d3dd0f2158eaab1234
Abhiram Krishnam <abhikrishnam01@gmail.com>
2025-07-12 18:57:21 -0400

    profile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,12 +13,16 @@ function Hero({}: Props) {
     })
   return (
     
-    <div>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
         <BGCircles/>
-        <h1>
+        <img className='relative rounded-full h-50 w-50 mx-auto object-cover'
+        src="/pp.jpeg" alt="Abhiram" />
+        <div className="z-30">
+        <h1 className="text-xl lg:text-3xl font-sans px-5">
             <span>{text}</span>
             <Cursor cursorColor="™#F7AB0A" />
         </h1>
+        </div>
     </div>
   )
 }
