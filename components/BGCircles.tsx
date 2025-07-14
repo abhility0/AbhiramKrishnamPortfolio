4f29962f3f6a269e67525d6ac2e86b29d76b5013
Abhiram Krishnam <abhikrishnam01@gmail.com>
2025-07-14 16:47:29 -0400

    About

--- components/BGCircles.tsx
+++ components/BGCircles.tsx
@@ -20,7 +20,6 @@ function BGCircles({}: Props) {
     }}
     className='relative flex justify-center items-center'>
         <div className="absolute border border-[#333333] rounded-full h-[400px] w-[400px] mt-52 animate-ping" />
-        <div className=" rounded-full border border-[#333333] h-[600px] w-[600px] absolute mt-52" />
         <div className=" rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52" />
         <div className=" rounded-full border border-[#F7AB0A] opacity-20 h-[950px] w-[950px] absolute mt-52" />
         <div className="rounded-full border border-[#F7AB0A] opacity-20 h-[1200px] w-[1200px] absolute mt-52 animate-pulse"/>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BGCircles from './BGCircles'
+import Link from 'next/link'
 
 type Props = {}
 
 function Hero({}: Props) {
     const [text,count] = useTypewriter({
-        words: ["Hi, I'm Abhiram","A GenAI Engineer","But I can do other stuff too"],
+        words: ["Hi, I'm Abhiram","I like making websites, LLMs, websites with LLMs","You can tell by scrolling down"],
         loop: true,
-        delaySpeed: 1700
+        delaySpeed: 1600
 
     })
   return (
@@ -18,10 +19,28 @@ function Hero({}: Props) {
         <img className='relative rounded-full h-50 w-50 mx-auto object-cover'
         src="/pp.jpeg" alt="Abhiram" />
         <div className="z-30">
-        <h1 className="text-xl lg:text-3xl font-sans px-5">
-            <span>{text}</span>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+            Software Engineer
+        </h2>
+        <h1 className=" text-4xl lg:text-3xl font-semibold scroll-px-10">
+            <span className="mr-3">{text}</span>
             <Cursor cursorColor="™#F7AB0A" />
         </h1>
+
+        <div className='pt-5'>
+            <Link href="#about">
+                <button className='heroButton'>About</button>
+            </Link >
+            <Link href="#experience">
+                <button className='heroButton'>Experience</button>
+            </Link>
+            <Link href="#skills">
+                <button className='heroButton'>Skills</button>
+            </Link>
+            <Link href="#projects">
+                <button className='heroButton'>Projects</button>
+            </Link>
+        </div>
         </div>
     </div>
   )
