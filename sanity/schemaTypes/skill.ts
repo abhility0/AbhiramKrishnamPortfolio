3b57c2ed0e2bc3288e14b42425fbb8de06546d56
Abhiram Krishnam <abhikrishnam01@gmail.com>
2025-08-01 16:08:21 -0400

    skilll

---
+++ sanity/schemaTypes/skill.ts
@@ -0,0 +1,24 @@
+import {DocumentTextIcon} from '@sanity/icons'
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const pageInfo = defineType({
+    name:"skill", 
+    title: "Skill", 
+    type: "document",
+  icon: DocumentTextIcon,
+  fields: [
+    defineField({
+      name:"title", title: "title", description: "Title of the skill", type: "string"
+    }),
+    defineField({
+        name:"progress", title: "Progres", type: "number",description: "Progress of skill from 0 to 100%",
+        validation: (Rule) => Rule.min(0).max(100),
+    }),
+    defineField({
+        name:"image", title: "Image", type: "image",
+        options: {
+            hotspot: true,
+        }
+    }),
+  ]
+})
